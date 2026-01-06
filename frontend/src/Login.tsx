3cf0fd643a46a829716e374c6543f1fc2bde0edf
rohit2195-jpg <rsattulu@purdue.edu>
2026-01-06 08:47:04 -0500

    fixed login path

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -4,7 +4,7 @@ function Login() {
     return (
         <div className="App">
             <header className="App-header">
-                <a className="btn-spotify" href="/auth/login" >
+                <a className="btn-spotify" href="https://main.d3db2kmtelhv4j.amplifyapp.com/auth/login" >
                     Login with Spotify 
                 </a>
             </header>
