a42edbcef4c71eae31b8694defd6c324aee26783
rohit2195-jpg <rsattulu@purdue.edu>
2026-01-06 13:54:12 -0500

    redirect uri 3

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ console.log(spotify_client_id);
 
 let access_token = null;
 
-const redirect_uri = "https://main.d3db2kmtelhv4j.amplifyapp.com/auth/callback"
+const redirect_uri = "http://127.0.0.1:5174/auth/callback"
 
 
 app.use(cors());
