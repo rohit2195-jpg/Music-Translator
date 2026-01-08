c3d07917283a7da3c6f9709f749e924960dd13c3
rohit2195-jpg <rsattulu@purdue.edu>
2026-01-08 10:28:18 -0500

    redirect uri2

--- backend/server.js
+++ backend/server.js
@@ -303,7 +303,7 @@ app.get('/auth/callback', async (req, res) => {
     // this line
     // http://music-translator-app-frontend.s3-website.us-east-2.amazonaws.com
     // http://127.0.0.1:5174/
-    res.redirect("dukz8h8o1lsds.cloudfront.net");
+    res.redirect("https://dukz8h8o1lsds.cloudfront.net");
 
   } catch (error) {
     console.error('Error fetching Spotify token:', error);
